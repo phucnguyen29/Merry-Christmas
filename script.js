e36711086b2c7f6b002b328f209f0ef9100528c0
PhucNguyen29 <112948401+phucnguyen29@users.noreply.github.com>
2022-12-11 10:10:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector("#btn").onclick = function () {
+  document.querySelector("#btn").style.display = "none";
+  document.querySelector(".christmas").style.display = "block";
+  document.querySelector("#body").style.background = "#de2f32";
+};
