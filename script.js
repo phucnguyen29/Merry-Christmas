54ced670e75074febdf7c28bbc25d92be5651eee
PhucNguyen29 <112948401+phucnguyen29@users.noreply.github.com>
2022-12-11 10:42:35 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ document.querySelector("#btn").onclick = function () {
   document.querySelector(".christmas").style.display = "block";
   document.querySelector("#body").style.background = "#de2f32";
 };
-// window.onload = function () {
-//   document.getElementById("autoplay").play();
-// };
+window.onload = function () {
+  document.getElementById("autoplay").play();
+};
+document.getElementById("body").addEventListener("click", () => {
+  document.getElementById("autoplay").play();
+});
